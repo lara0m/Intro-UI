21e22e9544734b21b45b250a646714e6b6229055
Lara Muiña <152939155+lara0m@users.noreply.github.com>
2025-04-21 12:04:50 -0300

    saludo hecho

---
+++ Assets/UI_Manager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Manager : MonoBehaviour
+{
+    public TextMeshProUGUI txtSaludo;
+    public TMP_InputField inputNombre;
+    string nombreIngresado;
+    string saludo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSaludo.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Saludar()
+    {
+        //obtener el nombre el input field
+        nombreIngresado = inputNombre.text;
+
+        //concatenar el nombre con el saludo
+        saludo = "Hola " + nombreIngresado + "!";
+
+        //mostrar el saludo en txtSaludo
+        txtSaludo.text = saludo;
+    }
+}
