27be04a853106c23e92003087d5bfc52e171c965
Lara Muiña <152939155+lara0m@users.noreply.github.com>
2025-04-23 15:39:00 -0300

    ingreso clave terminado

---
+++ Assets/ClaveManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClaveManager : MonoBehaviour
+{
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI txtMensaje;
+
+    string clave = "tic2025";
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtMensaje.text = "";
+
+    }
+    
+    // siempre se usa una funcion para conectar el codigo con los botones de Unity
+    public void ComprobarClaveIngresada()
+    {
+        string claveIngresada = inputClave.text;
+        if(claveIngresada != clave)
+        {
+            txtMensaje.text = "Acceso denegado";
+            inputClave.text = "";
+        } 
+        else
+        {
+            txtMensaje.text = "Bienvenido";
+        }
+    }
+}
